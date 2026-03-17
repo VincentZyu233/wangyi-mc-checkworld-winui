3a30e5285ce4b3b201261a68e8edf61c52dd4640
VincentZyuwin11台式机 <VincentZyuwin11>
2026-03-17 16:12:15 +0800

    build release: v0.1.10 - convert to code-only UI to avoid XAML compiler
    - Remove MainWindow.xaml and App.xaml files
    - Create UI programmatically in C# code
    - Initialize resources in code instead of XAML
    - Completely avoid XAML compilation issues
    - Version: 0.1.9  0.1.10

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,23 @@ public partial class App : Application
 {
     public App()
     {
-        this.InitializeComponent();
+        // Initialize resources programmatically instead of using XAML
+        var resources = new Microsoft.UI.Xaml.ResourceDictionary();
+
+        // Add XamlControlsResources
+        var xamlControlsResources = new Microsoft.UI.Xaml.Controls.XamlControlsResources();
+        resources.MergedDictionaries.Add(xamlControlsResources.MergedDictionaries[0]);
+
+        // Add custom colors
+        resources.Add("ApplicationBackgroundBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.White));
+        resources.Add("TextFillColorPrimaryBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Black));
+        resources.Add("TextFillColorSecondaryBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.Gray));
+        resources.Add("ControlFillColorDefaultBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.LightGray));
+        resources.Add("ControlStrongFillColorDefaultBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.LightGray));
+        resources.Add("ControlStrokeColorDefaultBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.DarkGray));
+        resources.Add("AccentFillColorDefaultBrush", new Microsoft.UI.Xaml.Media.SolidColorBrush(Microsoft.UI.Colors.CornflowerBlue));
+
+        this.Resources = resources;
     }
 
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace WangyiMCCheckworld;
 
@@ -7,5 +8,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         this.InitializeComponent();
+        this.Title = "网易MC检查世界";
+        this.Width = 1100;
+        this.Height = 700;
+
+        // Create the UI programmatically
+        var grid = new Grid();
+        var textBlock = new TextBlock
+        {
+            Text = "Hello World",
+            HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center,
+            VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center
+        };
+
+        grid.Children.Add(textBlock);
+        this.Content = grid;
     }
 }
