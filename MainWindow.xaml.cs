d9a91565d29fedcf8a035da2b2a9b69b3926a3cb
VincentZyuwin11台式机 <VincentZyuwin11>
2026-03-17 16:24:04 +0800

    build release: v0.1.13 - fix window sizing using AppWindow API
    - Use AppWindow.Resize() instead of Width/Height properties
    - Add Microsoft.Windows.SDK.Win32Metadata package for Win32 interop
    - Add necessary using statements for window sizing
    - Version: 0.1.12  0.1.13

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI;
 
 namespace WangyiMCCheckworld;
 
@@ -8,8 +9,12 @@ public class MainWindow : Window
     public MainWindow()
     {
         this.Title = "网易MC检查世界";
-        this.Width = 1100;
-        this.Height = 700;
+
+        // Set window size using AppWindow
+        var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hwnd);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 1100, Height = 700 });
 
         // Create the UI programmatically
         var grid = new Grid();
